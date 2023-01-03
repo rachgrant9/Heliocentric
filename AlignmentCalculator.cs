ea552994418a91e612f47bbd7839b96e1ad7227e
Rachel Grant <rachel.grant:@derivco.co.im>
2023-01-03 11:23:45 +0000

    chore: add heliocentric code to git

---
+++ AlignmentCalculator.cs
@@ -0,0 +1,37 @@
+﻿namespace SDETQ1
+{
+  public class AlignmentCalculator
+  {
+    public int EarthCurrentDay;
+    public int MarsCurrentDay;
+    public int DaysTillAligned;
+    const int FirstDayYear = 0;
+
+    public AlignmentCalculator(int earth, int mars)
+    {
+      EarthCurrentDay = earth;
+      MarsCurrentDay = mars;
+      DaysTillAligned = CalculateWhenPlanetsAlign();
+    }
+
+    public int CalculateWhenPlanetsAlign()
+    {
+      // how many loops until they are both at 0
+      int numberIterations = 0;
+
+      while (!(MarsCurrentDay == FirstDayYear) || !(EarthCurrentDay == FirstDayYear))
+      {
+        // increase the day by 1
+        EarthCurrentDay++;
+        MarsCurrentDay++;
+
+        // current day is remainder of day/year
+        EarthCurrentDay %= 365;
+        MarsCurrentDay %= 687;
+        numberIterations++;
+      }
+
+      return numberIterations;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SDETQ1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var input = GetInput();
+
+      foreach (var item in input)
+      {
+        var lineNumber = input.IndexOf(item) + 1;
+        var earthDay = Convert.ToInt32(item.Split(" ")[0]);
+        var marsDay = Convert.ToInt32(item.Split(" ")[1]);
+
+        AlignmentCalculator calculateCase = new AlignmentCalculator(earthDay, marsDay);
+
+        string caseOutput = $"Case {lineNumber}: ";
+        string result = Convert.ToString(calculateCase.DaysTillAligned);
+        Console.WriteLine(caseOutput + result);
+      }
+    }
+
+    private static List<string> GetInput()
+    {
+      string line;
+      List<string> input = new List<string>();
+      var inputLine = 1;
+      bool isEndFile = false;
+      while (inputLine <= 10 && !isEndFile)
+      {
+        line = Console.ReadLine();
+        if (String.IsNullOrEmpty(line))
+        {
+          isEndFile = true;
+        }
+        else
+        {
+          input.Add(line);
+          inputLine++;
+        }
+      }
+
+      return input;
+    }
+  }
+}
